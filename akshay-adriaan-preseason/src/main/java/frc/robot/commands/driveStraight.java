b9c899bd049abd7c0b53459782efb82c1ed4844a
sanjana36 <sanjana.la36@gmail.com>
2021-11-11 20:23:07 -0500

    driveStraight command

---
+++ akshay-adriaan-preseason/src/main/java/frc/robot/commands/driveStraight.java
@@ -0,0 +1,52 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.DriveSubsystem;
+
+public class driveStraight extends CommandBase {
+  private double speed;
+  private double distance;
+  public DriveSubsystem driveSubsystem;
+  
+  /** Creates a new driveStraight. */
+  public driveStraight(double speed, double distance, DriveSubsystem driveSubsystem) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(driveSubsystem);
+    this.driveSubsystem = driveSubsystem;
+  }
+
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    //start from a known state
+    driveSubsystem.drive(0,0);
+    driveSubsystem.resetEncoders();
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() { // TODO is broken, need compatibality
+    if (driveSubsystem.meanSpeed() < distance) {
+      
+    }
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    if (driveSubsystem.meanSpeed() == distance) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
--- akshay-adriaan-preseason/src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ akshay-adriaan-preseason/src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -11,25 +11,56 @@ import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class DriveSubsystem extends SubsystemBase {
-  private WPI_TalonSRX leftFront;
+  private static WPI_TalonSRX leftFront;
   private WPI_TalonSRX leftRear;
-  private WPI_TalonSRX rightFront;
+  private static WPI_TalonSRX rightFront;
   private WPI_TalonSRX rightRear;
-  
+
   private DifferentialDrive drive;
-  
+
   /** Creates a new DriveSubsystem. */
   public DriveSubsystem() {
-    leftFront = createAndConfigureMotor(2, false);
-    leftRear = createAndConfigureMotor(1, false);
-    rightFront = createAndConfigureMotor(4, true);
-    rightRear = createAndConfigureMotor(5, true);
+    WPI_TalonSRX leftFront = createAndConfigureMotor(2, false);
+    WPI_TalonSRX leftRear = createAndConfigureMotor(1, false);
+    WPI_TalonSRX rightFront = createAndConfigureMotor(4, true);
+    WPI_TalonSRX rightRear = createAndConfigureMotor(5, true);
 
     SpeedControllerGroup leftSide = new SpeedControllerGroup(leftFront, leftRear);
     SpeedControllerGroup rightSide = new SpeedControllerGroup(rightFront, rightRear);
-  
+
     drive = new DifferentialDrive(leftSide, rightSide);
     drive.setRightSideInverted(false);
+  }
+
+  public double getLeftFrontPosition() {
+    return leftFront.getSelectedSensorPosition();
+  
+  }
+
+  //public double getLeftRearPosition() {
+    //return leftRear.getSelectedSensorPosition();
+
+
+  public double getRightFrontPosition(){
+    return rightFront.getSelectedSensorPosition();
+  }
+
+  //public double getRightRearPosition() {
+    //return rightRear.getSelectedSensorPosition();
+    {
+  }
+  
+
+  public double meanSpeed() {
+    return (getRightFrontPosition() + getLeftFrontPosition())/2;
+
+  }
+
+  public void resetEncoders() {
+    
+    rightFront.setSelectedSensorPosition(0);
+    leftFront.setSelectedSensorPosition(0);
+
   }
 
   private WPI_TalonSRX createAndConfigureMotor(int canID, boolean invert) {
